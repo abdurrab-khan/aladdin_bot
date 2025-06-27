2fbf76d6668fda58db3d8d6a56dcf0ff50fce228
Abdur Rab Khan <abdurrabkhan222@gmail.com>
2025-06-27 22:00:35 +0530

    fix: Update PRODUCT_URL_CACHE_KEY for consistency in URL caching

--- src/constants/redis_key.py
+++ src/constants/redis_key.py
@@ -1,3 +1,3 @@
 PRODUCT_CATEGORY_CACHE_KEY = "product_categories_history_{category}"
-PRODUCT_URL_CACHE_KEY = "product_url_cache_"
+PRODUCT_URL_CACHE_KEY = "url_cache_"
 PRODUCT_URL_EXPIRE_TIME = 60 * 60 * 24 * 40
